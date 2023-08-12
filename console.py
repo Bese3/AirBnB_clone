7f88c06cbd410d339368b6280b0b2d4f571e276b
besufikad <07yilmabese@gmail.com>
2023-08-12 13:09:05 +0300

    console

--- console.py
+++ console.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import cmd
-from models.base_model import BaseModel
-import models
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+# import cmd
+# from models.base_model import BaseModel
+# import models
+# from models.user import User
+# from models.place import Place
+# from models.state import State
+# from models.city import City
+# from models.amenity import Amenity
+# from models.review import Review
 """Defines the HBnB console."""
 
 
